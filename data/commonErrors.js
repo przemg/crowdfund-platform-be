8197cb7c2e5e996fd5dac5e57c111951789d7dd7
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-07-25 21:45:11 +0200

    feat: add errorMiddleware

---
+++ data/commonErrors.js
@@ -0,0 +1,5 @@
+export const SERVER_ERROR = {
+  status: 500,
+  name: 'ServerError',
+  message: "'Server has encountered a problem and cannot complete the request'",
+};
--- server.js
+++ server.js
@@ -5,8 +5,31 @@ import config from './config/index.js';
 // Simple async/await error handling
 import 'express-async-errors';
 
+import { SERVER_ERROR } from './data/commonErrors.js';
+
+// Generic error handler for situations where we didn't handle errors properly
+const errorMiddleware = (error, req, res, next) => {
+  // When we add a custom error handler, we need to delegate to the default Express
+  // error handler, when the headers have already been sent to the client
+  if (res.headersSent) {
+    return next(error);
+  }
+
+  let { status, name, message } = error;
+
+  logger.error(error);
+
+  status ??= SERVER_ERROR.status;
+  name ??= SERVER_ERROR.name;
+  message ??= SERVER_ERROR.message;
+
+  return res.status(status).json({ error: { name, message } });
+};
+
 const startServer = () => {
   const app = express();
+
+  app.use(errorMiddleware);
 
   app.listen(config.port, () => {
     logger.info(`Listening on port ${config.port}`);
