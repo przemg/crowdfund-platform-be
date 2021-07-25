76565c4771727e5f89a4daf745d3623277c24197
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-07-25 23:14:54 +0200

    feat: close server on exit

--- server.js
+++ server.js
@@ -26,13 +26,53 @@ const errorMiddleware = (error, req, res, next) => {
   return res.status(status).json({ error: { name, message } });
 };
 
+// Ensures we close the server in the event of an error
+// https://stackoverflow.com/a/14032965/971592
+const setupCloseOnExit = (server) => {
+  const exitHandler = async (options = {}) => {
+    await server
+      .close()
+      .then(() => {
+        logger.info('Server successfully closed');
+      })
+      .catch((e) => {
+        logger.warn('Something went wrong closing the server', e.stack);
+      });
+
+    if (options.exit) process.exit();
+  };
+
+  // Do something when app is closing
+  process.on('exit', exitHandler);
+
+  // Catches ctrl+c event
+  process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+
+  // Catches "kill pid" (for example: nodemon restart)
+  process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+  process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+
+  // Catches uncaught exceptions
+  process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+};
+
 const startServer = () => {
   const app = express();
 
   app.use(errorMiddleware);
 
-  app.listen(config.port, () => {
+  const server = app.listen(config.port, () => {
     logger.info(`Listening on port ${config.port}`);
+
+    // This block of code turns `server.close` into a promise API
+    const originalClose = server.close.bind(server);
+    server.close = () =>
+      new Promise((resolveClose) => {
+        originalClose(resolveClose);
+      });
+
+    // Ensures we close the server in the event of an error
+    setupCloseOnExit(server);
   });
 };
 
