f8560130f5354f0f08151f278c21950ae5054396
Przemysław Gwóźdź <przemekgwozdz98@gmail.com>
2021-08-04 18:17:42 +0200

    fix: repair csrf cookie settings

--- src/config/csrfConfig.js
+++ src/config/csrfConfig.js
@@ -1,8 +1,5 @@
 import config from './index.js';
 
 export const csrfConfig = {
-  cookie: true,
-  httpOnlny: true,
-  secure: config.env === 'production',
-  sameSite: true,
+  cookie: { httpOnly: true, secure: config.env === 'production', sameSite: true },
 };
